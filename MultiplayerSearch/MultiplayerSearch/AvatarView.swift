483a070f0fab557e2813ff3269a247dba4c75ef0
Thinh Le <thinhlefrs@gmail.com>
2020-07-13 21:18:16 +0700

    [Layer Animation] Chapter 13: Creating the bounce-off animations

--- MultiplayerSearch/MultiplayerSearch/AvatarView.swift
+++ MultiplayerSearch/MultiplayerSearch/AvatarView.swift
@@ -3,73 +3,88 @@ import QuartzCore
 
 @IBDesignable
 class AvatarView: UIView {
-  
-  //constants
-  let lineWidth: CGFloat = 6.0
-  let animationDuration = 1.0
-  
-  //ui
-  let photoLayer = CALayer()
-  let circleLayer = CAShapeLayer()
-  let maskLayer = CAShapeLayer()
-  let label: UILabel = {
-    let label = UILabel()
-    label.font = UIFont(name: "ArialRoundedMTBold", size: 18.0)
-    label.textAlignment = .center
-    label.textColor = UIColor.black
-    return label
-  }()
-  
-  //variables
-  @IBInspectable
-  var image: UIImage? = nil {
-    didSet {
-      photoLayer.contents = image?.cgImage
-    }
-  }
-  
-  @IBInspectable
-  var name: String? = nil {
-    didSet {
-      label.text = name
-    }
-  }
-  
-  var shouldTransitionToFinishedState = false
-  
-  override func didMoveToWindow() {
-    layer.addSublayer(photoLayer)
-    photoLayer.mask = maskLayer
-    layer.addSublayer(circleLayer)
-    addSubview(label)
-  }
-  
-  override func layoutSubviews() {
-    super.layoutSubviews()
     
-    guard let image = image else {
-      return
+    //constants
+    let lineWidth: CGFloat = 6.0
+    let animationDuration = 1.0
+    
+    //ui
+    let photoLayer = CALayer()
+    let circleLayer = CAShapeLayer()
+    let maskLayer = CAShapeLayer()
+    let label: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "ArialRoundedMTBold", size: 18.0)
+        label.textAlignment = .center
+        label.textColor = UIColor.black
+        return label
+    }()
+    
+    //variables
+    @IBInspectable
+    var image: UIImage? = nil {
+        didSet {
+            photoLayer.contents = image?.cgImage
+        }
     }
     
-    //Size the avatar image to fit
-    photoLayer.frame = CGRect(
-      x: (bounds.size.width - image.size.width + lineWidth)/2,
-      y: (bounds.size.height - image.size.height - lineWidth)/2,
-      width: image.size.width,
-      height: image.size.height)
+    @IBInspectable
+    var name: String? = nil {
+        didSet {
+            label.text = name
+        }
+    }
     
-    //Draw the circle
-    circleLayer.path = UIBezierPath(ovalIn: bounds).cgPath
-    circleLayer.strokeColor = UIColor.white.cgColor
-    circleLayer.lineWidth = lineWidth
-    circleLayer.fillColor = UIColor.clear.cgColor
+    var shouldTransitionToFinishedState = false
     
-    //Size the layer
-    maskLayer.path = circleLayer.path
-    maskLayer.position = CGPoint(x: 0.0, y: 10.0)
+    override func didMoveToWindow() {
+        layer.addSublayer(photoLayer)
+        photoLayer.mask = maskLayer
+        layer.addSublayer(circleLayer)
+        addSubview(label)
+    }
     
-    //Size the label
-    label.frame = CGRect(x: 0.0, y: bounds.size.height + 10.0, width: bounds.size.width, height: 24.0)
-  }
-  
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        guard let image = image else {
+            return
+        }
+        
+        //Size the avatar image to fit
+        photoLayer.frame = CGRect(
+            x: (bounds.size.width - image.size.width + lineWidth)/2,
+            y: (bounds.size.height - image.size.height - lineWidth)/2,
+            width: image.size.width,
+            height: image.size.height)
+        
+        //Draw the circle
+        circleLayer.path = UIBezierPath(ovalIn: bounds).cgPath
+        circleLayer.strokeColor = UIColor.white.cgColor
+        circleLayer.lineWidth = lineWidth
+        circleLayer.fillColor = UIColor.clear.cgColor
+        
+        //Size the layer
+        maskLayer.path = circleLayer.path
+        maskLayer.position = CGPoint(x: 0.0, y: 10.0)
+        
+        //Size the label
+        label.frame = CGRect(x: 0.0, y: bounds.size.height + 10.0, width: bounds.size.width, height: 24.0)
+    }
+    
+    func bounceOff(point: CGPoint, morphSize: CGSize) {
+        let originalCenter = center
+        UIView.animate(withDuration: animationDuration, delay: 0.0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.0, animations: {
+            self.center = point
+        },completion: { _ in
+            //complete bounce to
+        } )
+        
+        UIView.animate(withDuration: animationDuration, delay: animationDuration, usingSpringWithDamping: 0.7, initialSpringVelocity: 1.0, animations: {
+            self.center = originalCenter }, completion: { _ in
+                delay(seconds: 0.1) {
+                    self.bounceOff(point: point, morphSize: morphSize)
+                }
+        })
+    }
 }
