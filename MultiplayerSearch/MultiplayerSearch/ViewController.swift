57d85057cf030594d4befee76351c365a9bc8751
Thinh Le <thinhlefrs@gmail.com>
2020-07-16 21:57:12 +0700

    [Layer Animation] Chapter 13: Challenge 1

--- MultiplayerSearch/MultiplayerSearch/ViewController.swift
+++ MultiplayerSearch/MultiplayerSearch/ViewController.swift
@@ -27,6 +27,28 @@ class ViewController: UIViewController {
         let leftBouncePoint = CGPoint(x: view.frame.size.width/2.0 - bounceXOffset, y: myAvatar.center.y)
         myAvatar.bounceOff(point: rightBouncePoint, morphSize: morphSize)
         opponentAvatar.bounceOff(point: leftBouncePoint, morphSize: morphSize)
+        delay(seconds: 4.0, completion: foundOpponent)
+    }
+    
+    func foundOpponent() {
+        status.text = "Connecting..."
+        opponentAvatar.image = UIImage(named: "avatar-2")
+        opponentAvatar.name = "Ray"
+        delay(seconds: 4.0, completion: connectedToOpponent)
+    }
+    
+    func connectedToOpponent() {
+        myAvatar.shouldTransitionToFinishedState = true
+        opponentAvatar.shouldTransitionToFinishedState = true
+        delay(seconds: 1.0, completion: completed)
+    }
+    
+    func completed() {
+        status.text = "Ready to play"
+        UIView.animate(withDuration: 0.2) {
+            self.vs.alpha = 1.0
+            self.searchAgain.alpha = 1.0
+        }
     }
     
     @IBAction func actionSearchAgain() {
