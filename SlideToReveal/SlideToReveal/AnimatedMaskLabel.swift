727da8fe2bb501a43064fd9ea2248ba3554781af
Thinh Le <thinhlefrs@gmail.com>
2020-07-16 22:32:45 +0700

    [Layer Animation] Chapter 14: Draw first gradient

--- SlideToReveal/SlideToReveal/AnimatedMaskLabel.swift
+++ SlideToReveal/SlideToReveal/AnimatedMaskLabel.swift
@@ -3,28 +3,33 @@ import QuartzCore
 
 @IBDesignable
 class AnimatedMaskLabel: UIView {
-  
-  let gradientLayer: CAGradientLayer = {
-    let gradientLayer = CAGradientLayer()
     
-    // Configure the gradient here
+    let gradientLayer: CAGradientLayer = {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.5)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.5)
+        let colors = [UIColor.black.cgColor,
+                      UIColor.white.cgColor,
+                      UIColor.black.cgColor]
+        gradientLayer.colors = colors
+        let locations: [NSNumber] = [0.25, 0.5, 0.75]
+        gradientLayer.locations = locations
+        return gradientLayer
+    }()
     
-    return gradientLayer
-  }()
-  
-  @IBInspectable var text: String! {
-    didSet {
-      setNeedsDisplay()
+    @IBInspectable var text: String! {
+        didSet {
+            setNeedsDisplay()
+        }
     }
-  }
-  
-  override func layoutSubviews() {
-    layer.borderColor = UIColor.green.cgColor
-  }
-  
-  override func didMoveToWindow() {
-    super.didMoveToWindow()
     
-  }
-  
+    override func layoutSubviews() {
+        gradientLayer.frame = bounds
+        layer.borderColor = UIColor.green.cgColor
+    }
+    
+    override func didMoveToWindow() {
+        super.didMoveToWindow()
+        layer.addSublayer(gradientLayer)
+    }
 }
