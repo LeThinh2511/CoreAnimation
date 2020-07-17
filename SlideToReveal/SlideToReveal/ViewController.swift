4197c3737dcd5e387f360e1a17059dc04fabacbe
Thinh Le <thinhlefrs@gmail.com>
2020-07-17 21:27:55 +0700

    [Layer Animation] Chapter 14: Slide to reveal gesture reconigzer

--- SlideToReveal/SlideToReveal/ViewController.swift
+++ SlideToReveal/SlideToReveal/ViewController.swift
@@ -1,42 +1,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-  
-  @IBOutlet var slideView: AnimatedMaskLabel!
-  @IBOutlet var time: UILabel!
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
     
-  }
-  
-  func didSlide() {
+    @IBOutlet var slideView: AnimatedMaskLabel!
+    @IBOutlet var time: UILabel!
     
-    // reveal the meme upon successful slide
-    let image = UIImageView(image: UIImage(named: "meme"))
-    image.center = view.center
-    image.center.x += view.bounds.size.width
-    view.addSubview(image)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let swipe = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.didSlide))
+        swipe.direction = .right
+        slideView.addGestureRecognizer(swipe)
+    }
     
-    UIView.animate(withDuration: 0.33, delay: 0.0,
-      animations: {
-        self.time.center.y -= 200.0
-        self.slideView.center.y += 200.0
-        image.center.x -= self.view.bounds.size.width
-      },
-      completion: nil
-    )
-    
-    UIView.animate(withDuration: 0.33, delay: 1.0,
-      animations: {
-        self.time.center.y += 200.0
-        self.slideView.center.y -= 200.0
-        image.center.x += self.view.bounds.size.width
-      },
-      completion: {_ in
-        image.removeFromSuperview()
-      }
-    )
-  }
-  
+    @objc func didSlide() {
+        
+        // reveal the meme upon successful slide
+        let image = UIImageView(image: UIImage(named: "meme"))
+        image.center = view.center
+        image.center.x += view.bounds.size.width
+        view.addSubview(image)
+        
+        UIView.animate(withDuration: 0.33, delay: 0.0, animations: {
+            self.time.center.y -= 200.0
+            self.slideView.center.y += 200.0
+            image.center.x -= self.view.bounds.size.width
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 0.33, delay: 1.0, animations: {
+            self.time.center.y += 200.0
+            self.slideView.center.y -= 200.0
+            image.center.x += self.view.bounds.size.width
+        }, completion: {_ in
+            image.removeFromSuperview()
+        })
+    }
 }
