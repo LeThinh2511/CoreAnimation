3c665ed9daa2e2578e15955518946fa83763fe3f
Thinh Le <thinhlefrs@gmail.com>
2020-07-19 21:34:16 +0700

    [Layer Animation] Chapter 16: Setting up the replicator layer

--- Iris/Iris/ViewController.swift
+++ Iris/Iris/ViewController.swift
@@ -8,11 +8,24 @@ class ViewController: UIViewController {
     
     let monitor = MicMonitor()
     let assistant = Assistant()
+    let replicator = CAReplicatorLayer()
+    let dot = CALayer()
+    let dotLength: CGFloat = 6.0
+    let dotOffset: CGFloat = 8.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        replicator.frame = view.bounds
+        view.layer.addSublayer(replicator)
         
-        
+        dot.frame = CGRect(x: replicator.frame.size.width - dotLength, y: replicator.position.y, width: dotLength, height: dotLength)
+        dot.backgroundColor = UIColor.lightGray.cgColor
+        dot.borderColor = UIColor(white: 1.0, alpha: 1.0).cgColor
+        dot.borderWidth = 0.5
+        dot.cornerRadius = 1.5
+        replicator.addSublayer(dot)
+        replicator.instanceCount = Int(view.frame.size.width / dotOffset)
+        replicator.instanceTransform = CATransform3DMakeTranslation(-dotOffset, 0.0, 0.0)
     }
     
     @IBAction func actionStartMonitoring(_ sender: AnyObject) {
