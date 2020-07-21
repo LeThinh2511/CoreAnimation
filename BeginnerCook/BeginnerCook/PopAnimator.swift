b3eba55e6058fb1350499c34ab1cf77430125d88
Thinh Le <thinhlefrs@gmail.com>
2020-07-21 21:25:07 +0700

    [ViewController transition animation] Chapter 17: Creating transition animator

--- BeginnerCook/BeginnerCook/PopAnimator.swift
+++ BeginnerCook/BeginnerCook/PopAnimator.swift
@@ -9,8 +9,12 @@
 import UIKit
 
 class PopAnimator: NSObject, UIViewControllerAnimatedTransitioning {
+    let duration = 1.0
+    var presenting = true
+    var originFrame = CGRect.zero
+    
     func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
-        return 0
+        return duration
     }
     
     func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
