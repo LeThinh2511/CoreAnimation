69963bd68db48b2225beb6e381db92918ae295ae
Thinh Le <thinhlefrs@gmail.com>
2020-07-22 21:42:04 +0700

    [ViewController transition animation] Chapter 17: smooth the transition animation

--- BeginnerCook/BeginnerCook/PopAnimator.swift
+++ BeginnerCook/BeginnerCook/PopAnimator.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class PopAnimator: NSObject, UIViewControllerAnimatedTransitioning {
-    let duration = 1.0
+    let duration = 2.0
     var presenting = true
     var originFrame = CGRect.zero
     var dismissCompletion: (()->Void)?
@@ -20,8 +20,15 @@ class PopAnimator: NSObject, UIViewControllerAnimatedTransitioning {
     
     func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
         let containerView = transitionContext.containerView
-        let herbView = presenting ? transitionContext.view(forKey: .to)! : transitionContext.view(forKey: .from)!
         
+        
+        guard let herbView = presenting ? transitionContext.view(forKey: .to) : transitionContext.view(forKey: .from),
+            let herbViewController = (presenting ? transitionContext.viewController(forKey: .to) : transitionContext.viewController(forKey: .from)) as? HerbDetailsViewController else {
+            return
+        }
+        
+        let finalAlpha: CGFloat = presenting ? 1 : 0
+        let initialAlpha: CGFloat = presenting ? 0 : 1
         let initialFrame = presenting ? originFrame : herbView.frame
         let finalFrame = presenting ? herbView.frame : originFrame
         let xScaleFactor = presenting ? initialFrame.width / finalFrame.width : finalFrame.width / initialFrame.width
@@ -37,7 +44,9 @@ class PopAnimator: NSObject, UIViewControllerAnimatedTransitioning {
         
         containerView.addSubview(herbView)
         containerView.bringSubviewToFront(herbView)
+        herbViewController.containerView.alpha = initialAlpha
         UIView.animate(withDuration: duration, delay:0.0, usingSpringWithDamping: 0.4, initialSpringVelocity: 0.0, animations: {
+            herbViewController.containerView.alpha = finalAlpha
             herbView.transform = self.presenting ? CGAffineTransform.identity : scaleTransform
             herbView.center = CGPoint(x: finalFrame.midX, y: finalFrame.midY)
         }, completion: { _ in
