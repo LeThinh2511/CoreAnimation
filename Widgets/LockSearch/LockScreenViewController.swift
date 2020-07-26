b120ef090f93c218d5f66d0bdd3f7c476c113b8f
Thinh Le <thinhlefrs@gmail.com>
2020-07-26 20:54:21 +0700

    [UIViewPropertyAnimator] Chapter 20: Adding animation

--- Widgets/LockSearch/LockScreenViewController.swift
+++ Widgets/LockSearch/LockScreenViewController.swift
@@ -30,6 +30,12 @@ class LockScreenViewController: UIViewController {
   
   override func viewDidAppear(_ animated: Bool) {
     let scale = UIViewPropertyAnimator(duration: 0.33, curve: .easeIn)
+    scale.addAnimations {
+      self.tableView.alpha = 1.0
+    }
+    scale.addAnimations({
+      self.tableView.transform = .identity
+    }, delayFactor: 0.33)
   }
   
   override func viewWillLayoutSubviews() {
